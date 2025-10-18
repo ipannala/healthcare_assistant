db2a47c61e13620f9c65d46fde5bf435f0754af4
ipannala <66149572+ipannala@users.noreply.github.com>
2025-10-18 11:27:58 -0700

    Key words dictionary used in router_intent function

---
+++ key_words.py
@@ -0,0 +1,4 @@
+BOOKING_KEY_WORDS = {'book', 'schedule', 'appointment'}
+RETRIEVE_KEY_WORDS = {'retrieve', 'recover', 'summarize', 'summary'}
+SEARCH_KEY_WORDS = {'search', 'lookup', 'look', 'find'}
+HISTORY_KEY_WORDS = {'add', 'update', 'append'}
